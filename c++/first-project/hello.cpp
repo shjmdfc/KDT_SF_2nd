f9eb584f558bd784ddbca84efb1002d2efd7a14d
shjmdfc <shjmdfc@gmail.com>
2023-03-23 17:24:42 +0900

    feat: 230323 coding lesson

---
+++ c++/first-project/hello.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, world!!!!!!!" << "\n";
+    int a = 1;
+    int b = 2;
+    std::cout << a+b;
+    std::cout << "\n";
+
+    float f = 1.2;
+float g = 3.9;
+std::cout << f+ g;
+}
+
